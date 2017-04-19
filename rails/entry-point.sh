68fdf1ab19f2a05d1312376ed76c45d320cfd7df
indrajit <eendroroy@gmail.com>
2017-04-19 12:03:18 +0600

    use bundle to run server

--- rails/entry-point.sh
+++ rails/entry-point.sh
@@ -1,4 +1,4 @@
 RAILS_ENV=production bundle exec rake assets:precompile
 RAILS_ENV=production bundle exec rake db:setup
 RAILS_ENV=production bundle exec rake db:migrate
-RAILS_ENV=production rails server -b 0.0.0.0 -p 666
+RAILS_ENV=production bundle exec rails server -b 0.0.0.0 -p 666
